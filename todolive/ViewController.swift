a6a1554d48e6c99105321162f4bdb9e48e948403
Billion Goenawan <billion.goenawan@mokapos.com>
2021-06-23 10:17:28 +0700

    Initial Commit

---
+++ todolive/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  todolive
+//
+//  Created by Billion MOKAPOS on 23/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
